f9bfb6b40e06ceda982e7eae1046319bb8689357
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-01 07:35:44 +0300

    Update array_01.cpp

--- tuple_interface/array_01.cpp
+++ tuple_interface/array_01.cpp
@@ -5,7 +5,6 @@ int main()
 {
 	using ar_5i = std::array<int, 5>;
 
-	constexpr auto sz = std::tuple_size_v<ar_5i>;  //sz = 5u
 	//std::tuple_element<0, ar_5i>::type x{}; //int x{};
 	std::tuple_element_t<0, ar_5i>x{}; //int x{};
 }
