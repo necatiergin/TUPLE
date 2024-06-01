c6ff39013ac7568fee8c01e5c80b6b2c3bfae2fb
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-01 07:49:43 +0300

    Update custom_01.cpp

--- tuple_interface/custom_01.cpp
+++ tuple_interface/custom_01.cpp
@@ -1,27 +1,36 @@
 #include <array>
 #include <tuple>
+#include <type_traits>
 
-class Date{
+class Date {
 public:
 
 private:
 	int m_day, m_mon, m_year;
 };
 
+template<std::size_t N>
+struct std::tuple_element<N, Date>
+{
+	static_assert(N < 3);
+	using type = int;
+};
+
 
 template <>
 struct std::tuple_size<Date>
 {
-	static constexpr std::size_t value{3u};
+	static constexpr std::size_t value{ 3u };
 };
-
-template <>
-constexpr std::size_t std::tuple_size_v<Date> = std::tuple_size<Date>::value;
 
 
 int main()
 {
 	//constexpr auto n = std::tuple_size<Date>::value;
 	constexpr auto n = std::tuple_size_v<Date>;
+	
+	static_assert(std::is_same_v<std::tuple_element_t<0, Date>, int>);
+	static_assert(std::is_same_v<std::tuple_element_t<1, Date>, int>);
+	static_assert(std::is_same_v<std::tuple_element_t<2, Date>, int>);
+	
 }
-
